44d389022e222c728f2f5419ec284c7ad3f0d62b
Thomas Levine <thomas.levine@gmail.com>
2011-02-26 11:41:36 -0500

    another way of saving n-grams

--- save_ngrams.py
+++ save_ngrams.py
@@ -1,5 +1,6 @@
 #Save unigrams to a file.
 #Then create n>1 n-grams and save those to a file.
+#Ooops you already wrote a better version of this
 import filters
 
 TRAIN='Shakespeare/Train.txt'
@@ -9,8 +10,15 @@ TRI='Shakespeare/trigrams.txt'
 
 uni=filters.shakespeare(TRAIN)
 
+#Unigrams
 g=open(UNI,'w')
 for i in range(0,len(uni)-1):
 	g.write(uni[i]+'\n')
 g.close()
 
+
+n=3
+g=open(TRI,'w')
+for i in range(n,len(uni)):
+	g.write(uni[i:i+n+1]+'\n')
+g.close()
