1479c6bbc68fb71da66326a5b12ff4b1428e09a3
Alec Story <avs38@cornell.edu>
2011-02-26 20:25:57 -0500

    probability calculator

--- ngram.py
+++ ngram.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 from random import normalvariate #Just for generating numbers for testing functions
 from math import log,e
+from fractions import Fraction
+
+use_fractions = True
+
+def dict_sum(d):
+	s = 0
+	for k in d.keys():
+		s += d[k]
+	return s
 
 def ngram(n, words):
-	"""Return a list of n (1 .. n)-gram dictionaries, n >= 1, l[0] is {}."""
+	"""Return a list of n (1 .. n)-gram dictionaries, n >= 1, l[0] is {}, where
+	the keys are tuples of words"""
 	ngrams = [{}]
 	for i in range(1, n+1):
 		d = {}
@@ -18,8 +28,28 @@ def ngram(n, words):
 				d[t] = 1
 			word_buffer.pop(0)
 		ngrams.append(d)
+	
 	return ngrams
 
+def probabilities(ngrams):
+	totals = [dict_sum(x) for x in ngrams]
+	probabilities = {}
+	ngram = ngrams[-1]
+	prevgram = ngrams[-2]
+	for k in ngram.keys():
+		num = ngram[k]
+		if len (ngrams) > 2:
+			denom = prevgrams[k[0:-1]]
+		else:
+			denom = totals[1]
+		import code
+		code.interact(local=locals())
+		print "num: %s, denom: %s" % (num, denom)
+		if use_fractions:
+			probabilities[k] = Fraction(num, denom)
+		else:
+			probabilities[k] = log(num) - log(denom)
+	return probabilities
 
 #Small things that we need to add at some point
 def smooth_addone(ngrams):
