b32be926afe3f823344f62f3319899e95958d2ab
Thomas Levine <thomas.levine@gmail.com>
2011-02-28 00:05:51 -0500

    plots of perplexity of generated sentences

--- paper/analysis.R
+++ paper/analysis.R
@@ -4,4 +4,19 @@ get_results=function(file) {read.csv(file,sep='|',colClasses=c('integer','logica
 results.tolstoy=get_results('War and Peace results.csv')
 
 foo=results.tolstoy
-par(mfrow=c(1,2));for (i in c(0,1)){plot((perplexity)~n,data=subset(foo,use.fractions==i),main=paste('use fractions =',i))}
+foo.ag=aggregate(foo$perplexity,list(use.fractions=foo$use.fractions,n=foo$n),mean)
+
+
+#par(mfrow=c(1,2));for (i in c(0,1)){plot((perplexity)~n,data=subset(foo,use.fractions==i),main=paste('use fractions =',i))}
+
+plot(perplexity~n,data=foo,type='n',axes=F,
+	main='Perplexity of generated sentences as a function of n-gram length and probability computation method',
+	xlab='n-gram length',
+	ylab='Perplexity'
+)
+axis(1,at=1:max(foo$n))
+axis(2)
+for (i in c(0,1)){
+	points(perplexity~n,data=subset(foo,use.fractions==i),pch=21,col=NA,bg=1+i)
+	lines(x~n,data=subset(foo.ag,use.fractions==i),col=1+i)
+}
