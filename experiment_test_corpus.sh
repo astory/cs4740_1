8fdeb2e403323850d29f5c1fcecbc0748bb53a91
Thomas Levine <thomas.levine@gmail.com>
2011-02-28 16:35:36 -0500

    ignore paper.aux

--- experiment_test_corpus.sh
+++ experiment_test_corpus.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
-for n in 1 2 3 4 5 6
-	do for corpus in 'Shakespeare' 'War and Peace'
+#TRAIN='Train.txt'
+#TEST='Test.txt'
+TRAIN='shorter.txt'
+TEST='short.txt'
+echo 'corpus, n-gram length, smoothing, prob, perplexity,sentence'
+for n in 1 #2 #3 4 5 6
+	do for corpus in 'Shakespeare' #'War and Peace'
 		#Header for csv file
-		do echo 'corpus, n-gram length, smoothing, prob, perplexity,sentence'
-		echo corpus ', ' $n ', ' 'on'  ', ' 'log' ', ' \
-		#Perplexity
-		`./ngram.py -n $n -t $corpus/Train.txt -sl -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
-		#Sentence
-		`./ngram.py -n $n -t $corpus/Train.txt -sl|tail -n 1`
+		do echo corpus ', ' $n ', ' 'on'  ', ' 'log' ', ' `./ngram.py -n $n -t $corpus/$TRAIN -sl -p $corpus/$TEST|grep '^Perplexity:'|cut -d ' ' -f 2` ', ' `./ngram.py -n $n -t $corpus/$TRAIN -slm|tail -n 1`
 		
-		echo corpus ', ' $n ', ' 'off' ', ' 'log' ', ' \
-		#Perplexity
-		`./ngram.py -n $n -t $corpus/Train.txt  -l -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
-		#Sentence
-		`./ngram.py -n $n -t $corpus/Train.txt  -l|tail -n 1`
+		echo    corpus ', ' $n ', ' 'off' ', ' 'log' ', ' `./ngram.py -n $n -t $corpus/$TRAIN  -l -p $corpus/$TEST|grep '^Perplexity:'|cut -d ' ' -f 2` ', ' `./ngram.py -n $n -t $corpus/$TRAIN  -lm|tail -n 1`
 		
-		echo corpus ', ' $n ', ' 'on'  ', ' 'ap'  ', ' \
-		#Perplexity
-		`./ngram.py -n $n -t $corpus/Train.txt  -s -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
-		#Sentence
-		`./ngram.py -n $n -t $corpus/Train.txt  -s|tail -n 1`
+		echo    corpus ', ' $n ', ' 'on'  ', ' 'ap'  ', ' `./ngram.py -n $n -t $corpus/$TRAIN  -s -p $corpus/$TEST|grep '^Perplexity:'|cut -d ' ' -f 2` ', ' `./ngram.py -n $n -t $corpus/$TRAIN  -sm|tail -n 1`
 		
-		echo corpus ', ' $n ', ' 'off' ', ' 'ap'  ', ' \
-		#Perplexity
-		`./ngram.py -n $n -t $corpus/Train.txt     -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
-		#Sentence
-		`./ngram.py -n $n -t $corpus/Train.txt    |tail -n 1`
+		echo    corpus ', ' $n ', ' 'off' ', ' 'ap'  ', ' `./ngram.py -n $n -t $corpus/$TRAIN     -p $corpus/$TEST|grep '^Perplexity:'|cut -d ' ' -f 2` ', ' `./ngram.py -n $n -t $corpus/$TRAIN   -m|tail -n 1`
 	done
 done
