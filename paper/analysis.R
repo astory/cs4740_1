86821c097c32c3abc2d05cece8c27b32e077e0a2
Thomas Levine <thomas.levine@gmail.com>
2011-02-27 21:37:48 -0500

    plot of perplexity by n and use_fractions

--- paper/analysis.R
+++ paper/analysis.R
@@ -1,1 +1,7 @@
 get_results=function(file) {read.csv(file,sep='|',colClasses=c('integer','logical','numeric','numeric','character'))}
+
+#results.shakespeare=get_results('Shakespeare results.csv')
+results.tolstoy=get_results('War and Peace results.csv')
+
+foo=results.tolstoy
+par(mfrow=c(1,2));for (i in c(0,1)){plot((perplexity)~n,data=subset(foo,use.fractions==i),main=paste('use fractions =',i))}
