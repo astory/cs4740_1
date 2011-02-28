af2cbe43a1d03ffd53cb7e0cd83d1d5bff5626ec
Thomas Levine <thomas.levine@gmail.com>
2011-02-28 16:07:52 -0500

    experiment

--- experiment_test_corpus.sh
+++ experiment_test_corpus.sh
@@ -2,10 +2,29 @@
 for n in 1 2 3 4 5 6
 	do for corpus in 'Shakespeare' 'War and Peace'
 		#Header for csv file
-		do echo 'corpus, n-gram length, smoothing, prob, perplexity'
-		echo corpus ', ' $n ', ' 'on'  ', ' 'log' ', ' `./ngram.py -n $n -t $corpus/Train.txt -p $corpus/Test.txt -sl|grep 'Perplexity:'|cut -d ' ' -f 2`
-		echo corpus ', ' $n ', ' 'off' ', ' 'log' ', ' `./ngram.py -n $n -t $corpus/Train.txt -p $corpus/Test.txt  -l|grep 'Perplexity:'|cut -d ' ' -f 2`
-		echo corpus ', ' $n ', ' 'on'  ', ' 'ap'  ', ' `./ngram.py -n $n -t $corpus/Train.txt -p $corpus/Test.txt -s |grep 'Perplexity:'|cut -d ' ' -f 2`
-		echo corpus ', ' $n ', ' 'off' ', ' 'ap'  ', ' `./ngram.py -n $n -t $corpus/Train.txt -p $corpus/Test.txt    |grep 'Perplexity:'|cut -d ' ' -f 2`
+		do echo 'corpus, n-gram length, smoothing, prob, perplexity,sentence'
+		echo corpus ', ' $n ', ' 'on'  ', ' 'log' ', ' \
+		#Perplexity
+		`./ngram.py -n $n -t $corpus/Train.txt -sl -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
+		#Sentence
+		`./ngram.py -n $n -t $corpus/Train.txt -sl|tail -n 1`
+		
+		echo corpus ', ' $n ', ' 'off' ', ' 'log' ', ' \
+		#Perplexity
+		`./ngram.py -n $n -t $corpus/Train.txt  -l -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
+		#Sentence
+		`./ngram.py -n $n -t $corpus/Train.txt  -l|tail -n 1`
+		
+		echo corpus ', ' $n ', ' 'on'  ', ' 'ap'  ', ' \
+		#Perplexity
+		`./ngram.py -n $n -t $corpus/Train.txt  -s -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
+		#Sentence
+		`./ngram.py -n $n -t $corpus/Train.txt  -s|tail -n 1`
+		
+		echo corpus ', ' $n ', ' 'off' ', ' 'ap'  ', ' \
+		#Perplexity
+		`./ngram.py -n $n -t $corpus/Train.txt     -p $corpus/Test.txt|grep 'Perplexity:'|cut -d ' ' -f 2` ', ' \
+		#Sentence
+		`./ngram.py -n $n -t $corpus/Train.txt    |tail -n 1`
 	done
 done
