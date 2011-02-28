eab6443d4f14d577b38f1aa99e5267bd33dcfdd7
Thomas Levine <thomas.levine@gmail.com>
2011-02-28 17:02:32 -0500

    perplexity experiment should work

--- experiment_test_corpus.sh
+++ experiment_test_corpus.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-#TRAIN='Train.txt'
-#TEST='Test.txt'
-TRAIN='shorter.txt'
-TEST='short.txt'
+#Calculate perplexities and
+#generate sentences with different
+	#different corpora
+	#different n-gram lengths
+	#smoothing on or off
+	#different probability handling
+
+TRAIN='Train.txt'
+TEST='Test.txt'
+#Note: This fails on really really small corpora.
+#TRAIN='shorter.txt'
+#TEST='short.txt'
 echo 'corpus, n-gram length, smoothing, prob, perplexity,sentence'
-for n in 1 #2 #3 4 5 6
-	do for corpus in 'Shakespeare' #'War and Peace'
+for n in 1 2 3 4 5 6
+	do for corpus in 'Shakespeare' 'War_and_Peace'
 		#Header for csv file
 		do echo corpus ', ' $n ', ' 'on'  ', ' 'log' ', ' `./ngram.py -n $n -t $corpus/$TRAIN -sl -p $corpus/$TEST|grep '^Perplexity:'|cut -d ' ' -f 2` ', ' `./ngram.py -n $n -t $corpus/$TRAIN -slm|tail -n 1`
 		
