7ff7eeeb92c2bf206dc1cc4ffbb17777bfc969c4
Thomas Levine <thomas.levine@gmail.com>
2011-02-28 13:35:57 -0500

    plots in paper

--- paper/analysis.R
+++ paper/analysis.R
@@ -4,8 +4,6 @@ get_results=function(file) {read.csv(file,sep='|',colClasses=c('integer','logica
 results.tolstoy=get_results('../War and Peace results.csv')
 
 #foo=results.tolstoy
-
-
 #par(mfrow=c(1,2));for (i in c(0,1)){plot((perplexity)~n,data=subset(foo,use.fractions==i),main=paste('use fractions =',i))}
 
 pp_gen.plot=function(foo){
