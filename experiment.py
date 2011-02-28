698ff04ea55fb405d7672581c4fddf0c47dbe4c7
Thomas Levine <thomas.levine@gmail.com>
2011-02-28 15:08:23 -0500

    change experiment input slightly

--- experiment.py
+++ experiment.py
@@ -47,7 +47,7 @@ def run(train,nmax,reps,out):
 def main():
 	nmax=3
 	reps=3
-	run('War and Peace/short.txt',nmax,reps,'War and Peace results.csv')
+	run('War and Peace/Train.txt',nmax,reps,'War and Peace results.csv')
 	run('Shakespeare/Train.txt',nmax,reps,'Shakespeare results.csv')
 
 if __name__ == '__main__':
